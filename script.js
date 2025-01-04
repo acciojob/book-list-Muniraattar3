e8a476a3307d844acc125fbeb6e38652dd868093
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-01-04 11:26:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const form = document.querySelector('#submit');
+const booklist = document.querySelector('#book-list');
+
+form.addEventListner('click',addBook);
+
+function addBook(e){
+	e.preventDefault();
+
+	const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    return;
+
+const row =document.createElement('tr');
+row.innerHTML =` 
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete">Clear</button></td>
+	`;
+booklist.appendChild(row);
+
+	document.querySelector('#title').value='';
+	document.querySelector('#author').value='';
+	document.querySelector('#isbn').value='';
+}
+
+booklist.addEventListner('click',removeBook);
+
+function removeBook(e) {
+	if(e.target.classList.contains('delete')){
+		const row= e.target.parentElement.parentElement;
+		booklist.removeChild(row);
+	}
+}
+
